70c00e506206dfe28a6df08e62ca8807cbf187e2
Gabrieldev067 <gabrielmac2901@gmail.com>
2025-04-07 10:52:38 -0400

    Chat de mensagem

---
+++ main.py
@@ -0,0 +1,25 @@
+#------------------------------SISTEMA DE CHAT DE MENSAGEM-----------------------------------------
+
+import os
+
+mensagens=[]
+
+nome=input('Nome: ')
+
+while True:
+    os.system('cls')
+
+    if len(mensagens)>0:
+        for m in mensagens:
+            print(m['nome'], "-", m['texto'])
+
+    print('__'*5)
+
+    texto=input('Mensagem: ')
+    if texto=='fim' or 'Fim':
+        break
+
+    mensagens.append({
+        'nome': nome,
+        'texto': texto
+    })
